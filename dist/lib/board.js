78ca8122cfc2152816e99f788e2cdeb19283bf3a
Fahmin Guliyev <baku.fehmin@gmail.com>
2025-01-27 19:32:19 +0100

    isPieceHanging function update

--- dist/lib/board.js
+++ dist/lib/board.js
@@ -1,21 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPieceHanging = exports.getDefenders = exports.getAttackers = exports.pieceValues = exports.promotions = void 0;
+exports.isPieceHanging =
+    exports.getDefenders =
+    exports.getAttackers =
+    exports.pieceValues =
+    exports.promotions =
+        void 0;
 const chess_js_1 = require("chess.js");
 exports.promotions = [undefined, "b", "n", "r", "q"];
 exports.pieceValues = {
-    "p": 1,
-    "n": 3,
-    "b": 3,
-    "r": 5,
-    "q": 9,
-    "k": Infinity,
-    "m": 0
+    p: 1,
+    n: 3,
+    b: 3,
+    r: 5,
+    q: 9,
+    k: Infinity,
+    m: 0,
 };
 function getBoardCoordinates(square) {
     return {
         x: "abcdefgh".indexOf(square.slice(0, 1)),
-        y: parseInt(square.slice(1)) - 1
+        y: parseInt(square.slice(1)) - 1,
     };
 }
 function getSquare(coordinate) {
@@ -25,16 +30,18 @@ function getAttackers(fen, square) {
     let attackers = [];
     let board = new chess_js_1.Chess(fen);
     let piece = board.get(square);
-    board.load(fen
-        .replace(/(?<= )(?:w|b)(?= )/g, piece.color == "w" ? "b" : "w")
-        .replace(/ [a-h][1-8] /g, " - "));
+    board.load(
+        fen
+            .replace(/(?<= )(?:w|b)(?= )/g, piece.color == "w" ? "b" : "w")
+            .replace(/ [a-h][1-8] /g, " - ")
+    );
     let legalMoves = board.moves({ verbose: true });
     for (let move of legalMoves) {
         if (move.to == square) {
             attackers.push({
                 square: move.from,
                 color: move.color,
-                type: move.piece
+                type: move.piece,
             });
         }
     }
@@ -43,38 +50,36 @@ function getAttackers(fen, square) {
     let pieceCoordinate = getBoardCoordinates(square);
     for (let xOffset = -1; xOffset <= 1; xOffset++) {
         for (let yOffset = -1; yOffset <= 1; yOffset++) {
-            if (xOffset == 0 && yOffset == 0)
-                continue;
+            if (xOffset == 0 && yOffset == 0) continue;
             let offsetSquare = getSquare({
                 x: Math.min(Math.max(pieceCoordinate.x + xOffset, 0), 7),
-                y: Math.min(Math.max(pieceCoordinate.y + yOffset, 0), 7)
+                y: Math.min(Math.max(pieceCoordinate.y + yOffset, 0), 7),
             });
             let offsetPiece = board.get(offsetSquare);
-            if (!offsetPiece)
-                continue;
-            if (offsetPiece.color == oppositeColour && offsetPiece.type == "k") {
+            if (!offsetPiece) continue;
+            if (
+                offsetPiece.color == oppositeColour &&
+                offsetPiece.type == "k"
+            ) {
                 oppositeKing = {
                     color: offsetPiece.color,
                     square: offsetSquare,
-                    type: offsetPiece.type
+                    type: offsetPiece.type,
                 };
                 break;
             }
         }
-        if (oppositeKing)
-            break;
+        if (oppositeKing) break;
     }
-    if (!oppositeKing)
-        return attackers;
+    if (!oppositeKing) return attackers;
     let kingCaptureLegal = false;
     try {
         board.move({
             from: oppositeKing.square,
-            to: square
+            to: square,
         });
         kingCaptureLegal = true;
-    }
-    catch (_a) { }
+    } catch (_a) {}
     if (oppositeKing && (attackers.length > 0 || kingCaptureLegal)) {
         attackers.push(oppositeKing);
     }
@@ -86,29 +91,34 @@ function getDefenders(fen, square) {
     let piece = board.get(square);
     let testAttacker = getAttackers(fen, square)[0];
     if (testAttacker) {
-        board.load(fen
-            .replace(/(?<= )(?:w|b)(?= )/g, testAttacker.color)
-            .replace(/ [a-h][1-8] /g, " - "));
+        board.load(
+            fen
+                .replace(/(?<= )(?:w|b)(?= )/g, testAttacker.color)
+                .replace(/ [a-h][1-8] /g, " - ")
+        );
         for (let promotion of exports.promotions) {
             try {
                 board.move({
                     from: testAttacker.square,
                     to: square,
-                    promotion: promotion
+                    promotion: promotion,
                 });
                 return getAttackers(board.fen(), square);
-            }
-            catch (_a) { }
+            } catch (_a) {}
         }
-    }
-    else {
-        board.load(fen
-            .replace(/(?<= )(?:w|b)(?= )/g, piece.color)
-            .replace(/ [a-h][1-8] /g, " - "));
-        board.put({
-            color: piece.color == "w" ? "b" : "w",
-            type: "q"
-        }, square);
+    } else {
+        board.load(
+            fen
+                .replace(/(?<= )(?:w|b)(?= )/g, piece.color)
+                .replace(/ [a-h][1-8] /g, " - ")
+        );
+        board.put(
+            {
+                color: piece.color == "w" ? "b" : "w",
+                type: "q",
+            },
+            square
+        );
         return getAttackers(board.fen(), square);
     }
     return [];
@@ -119,34 +129,67 @@ function isPieceHanging(lastFen, fen, square) {
     let board = new chess_js_1.Chess(fen);
     let lastPiece = lastBoard.get(square);
     let piece = board.get(square);
+
+    // Validate that the pieces exist before accessing their properties
+    if (!lastPiece || !piece) {
+        console.error(
+            `Invalid piece detected. lastPiece: ${lastPiece}, piece: ${piece}, square: ${square}`
+        );
+        return false; // If either piece is undefined, return false (not hanging)
+    }
+
     let attackers = getAttackers(fen, square);
     let defenders = getDefenders(fen, square);
-    if (exports.pieceValues[lastPiece.type] >= exports.pieceValues[piece.type] && lastPiece.color != piece.color) {
+
+    if (
+        exports.pieceValues[lastPiece.type] >=
+            exports.pieceValues[piece.type] &&
+        lastPiece.color !== piece.color
+    ) {
         return false;
     }
-    if (piece.type == "r"
-        && exports.pieceValues[lastPiece.type] == 3
-        && attackers.every(atk => exports.pieceValues[atk.type] == 3)
-        && attackers.length == 1) {
+
+    if (
+        piece.type === "r" &&
+        exports.pieceValues[lastPiece.type] === 3 &&
+        attackers.every((atk) => exports.pieceValues[atk.type] === 3) &&
+        attackers.length === 1
+    ) {
         return false;
     }
-    if (attackers.some(atk => exports.pieceValues[atk.type] < exports.pieceValues[piece.type])) {
+
+    if (
+        attackers.some(
+            (atk) =>
+                exports.pieceValues[atk.type] < exports.pieceValues[piece.type]
+        )
+    ) {
         return true;
     }
+
     if (attackers.length > defenders.length) {
         let minAttackerValue = Infinity;
         for (let attacker of attackers) {
-            minAttackerValue = Math.min(exports.pieceValues[attacker.type], minAttackerValue);
+            minAttackerValue = Math.min(
+                exports.pieceValues[attacker.type],
+                minAttackerValue
+            );
         }
-        if (exports.pieceValues[piece.type] < minAttackerValue
-            && defenders.some(dfn => exports.pieceValues[dfn.type] < minAttackerValue)) {
+        if (
+            exports.pieceValues[piece.type] < minAttackerValue &&
+            defenders.some(
+                (dfn) => exports.pieceValues[dfn.type] < minAttackerValue
+            )
+        ) {
             return false;
         }
-        if (defenders.some(dfn => exports.pieceValues[dfn.type] == 1)) {
+        if (defenders.some((dfn) => exports.pieceValues[dfn.type] === 1)) {
             return false;
         }
         return true;
     }
+
     return false;
 }
+
 exports.isPieceHanging = isPieceHanging;
